1f4571a3b389a0f10ca2c3f6ecbad47ea1cb3b72
lewiskimtai <lewis.kimtai@gmail.com>
2021-07-12 12:17:55 +0300

    drawing functions

---
+++ tictactoe.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import numpy as np
+
+size_of_board = 600
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
+symbol_X_color = '#EE4035'
+symbol_O_color = '#0492CF'
+Green_color = '#7BC043'
+
+class Tic_Tac_Toe():
+    #Initialization of Functions:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('TicTacToe')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        #Input from the user in form of clicks
+        self.window.bind('<Button-1>', self.click)
+        self.initialize_board()
+        self.player_X_turns = True
+        self.board_status = np.zeros(shape=(3, 3))
+
+        self.player_X_starts = True
+        self.reset_board = False
+        self.gameover = False
+        self.tie = False
+        self.X_wins = False
+        self.O_wins = False
+
+        self.X_score = 0
+        self.O_score = 0
+        self.tie_score = 0
+
+    def mainloop(self):
+        self.window.mainloop()
+
+    def initialize_board(self):
+        for i in range(2):
+            self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
+
+        for i in range(2):
+            self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+
+    def play_again(self):
+        self.initialize_board()
+        self.player_X_starts = not self.player_X_starts
+        self.player_X_turns = self.player_X_starts
+        self.board_status = np.zeros(shape=(3, 3))
+
+    #Drawing Functions:
+    # The modules required to draw required game based object on canvas 
+    def draw_0(self, logical_position):
+        logical_position = np.array(logical_position)        
+            
+
